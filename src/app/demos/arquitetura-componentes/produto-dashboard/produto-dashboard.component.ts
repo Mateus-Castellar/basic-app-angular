ab1be197c31977b375ffe0301a8a7fdaabd99ffa
Mateus Castellar <mateuscastellar@outlook.com>
2022-08-25 15:15:06 -0300

    feat: @ViewChild e @ViewChildren

--- src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
+++ src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { fromEvent, Observable } from 'rxjs';
+import { ProdutoDetalheComponent } from '../components/produto-card-detalhe.component';
+import { ProdutoCountComponent } from '../components/produto-count.component';
 import { Produto } from '../models/produto';
 
 @Component({
   selector: 'app-produto-dashboard',
   templateUrl: './produto-dashboard.component.html',
 })
-export class ProdutoDashboardComponent implements OnInit
+export class ProdutoDashboardComponent implements OnInit, AfterViewInit
 {
 
   produtos: Produto[];
+
+  @ViewChild(ProdutoCountComponent,{ static: false }) contador : ProdutoCountComponent;
+  @ViewChild('teste', { static: false }) mensagemTela: ElementRef;
+  @ViewChildren(ProdutoDetalheComponent) botoes: QueryList<ProdutoDetalheComponent>;
 
   constructor() { }
 
@@ -58,8 +65,24 @@ export class ProdutoDashboardComponent implements OnInit
     }];
   }
 
+  ngAfterViewInit(): void
+  {
+    console.log('obejeto do contador: ', this.contador.produtos );
+
+    let clickTexto: Observable<any> = fromEvent(this.mensagemTela.nativeElement,'click');
+    clickTexto.subscribe(() =>{alert('clicou no texto');
+      return;
+    });
+
+    console.log(this.botoes);
+    this.botoes.forEach(lbda => {
+      console.log(lbda.produto);
+    })
+  }
+
   //o evento ira receber um produto
-  mudarStatus(event: Produto){
+  mudarStatus(event: Produto)
+  {
     event.ativo = !event.ativo;//recebe o inverso do estado atual
   }
 
